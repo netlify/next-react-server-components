3dafc6783df7600d09a56ef8580564ac9fb564be
Matt Kane <m@mk.gg>
2022-03-22 16:57:35 +0000

    chore: switch to installed plugin

--- local-plugin/index.ts
+++
@@ -1,5 +0,0 @@
-import { NetlifyPlugin } from '@netlify/build'
-import { writeMiddleware } from './helpers'
-export const onBuild: NetlifyPlugin['onBuild'] = async ({ netlifyConfig }) => {
-  await writeMiddleware(netlifyConfig)
-}
